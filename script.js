05ae1ddf61ed0d596127d661bd11c20d90b08472
adamizgin <88751196+adamizgin@users.noreply.github.com>
2021-12-05 20:31:25 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,33 @@
 $(document).ready(function() {
-	
+	$(window).scroll(function() {
+		sT = $(this).scrollTop();
+		kH = $('#knowledge').height();
+		
+		if ($(window).width() > 1170) {
+			if (sT > kH) {
+				$('#know-html').css({
+					'opacity': '1',
+					'top': '0'
+				});
+				setTimeout(function() {
+					$('#know-css').css({
+						'opacity': '1',
+						'top': '0'
+					});
+					setTimeout(function() {
+						$('#know-js').css({
+							'opacity': '1',
+							'top': '0'
+						});
+						setTimeout(function() {
+							$('#know-ph').css({
+								'opacity': '1',
+								'top': '0'
+							});
+						}, 200);
+					}, 200);
+				}, 200);
+			}
+		}
+	});
 });
