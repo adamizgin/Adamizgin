6ee2baed6695f9b20b9cd2d341cfc1f46f6e1f0b
adamizgin <88751196+adamizgin@users.noreply.github.com>
2021-12-06 00:08:08 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 $(document).ready(function() {
 	$(window).scroll(function() {
 		sT = $(this).scrollTop();
-		kH = $('#knowledge').height();
+		kH = $("#knowledge").offset().top;
 		
 		if ($(window).width() > 1170) {
-			if (sT > kH) {
+			if (sT >= kH - 200) {
 				$('#know-html').css({
 					'opacity': '1',
 					'top': '0'
@@ -29,5 +29,30 @@ $(document).ready(function() {
 				}, 200);
 			}
 		}
+
+		if (sT >= 100) {
+			$('#header').css({
+				'background': '#5a10d0ff',
+				'border-bottom': '1px solid #0002',
+				'box-shadow': '0 0 5px 0 #0003'
+			});
+		} else {
+			$('#header').css({
+				'background': '#5a10d000',
+				'border-bottom': '1px solid #0000',
+				'box-shadow': '0 0 5px 0 #0000'
+			});
+		}
+	});
+
+	$('#about-button').click(function() {
+		$([document.documentElement, document.body]).animate({
+	        scrollTop: $("#about-me").offset().top / 1.25
+	    }, 500);
+	});
+	$('#know-button').click(function() {
+		$([document.documentElement, document.body]).animate({
+	        scrollTop: $("#knowledge").offset().top
+	    }, 500);
 	});
 });
